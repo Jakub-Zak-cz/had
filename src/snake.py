44d8f91d58c886f38f3c589e0d25bc528a5945b4
Jakub Žák <zakj23@spstrutnov.cz>
2024-01-09 09:32:28 +0100

    adding assets + pygame ready

--- src/snake.py
+++ src/snake.py
@@ -1,6 +1,30 @@
-# Snake the videogame coded with python
+# Snake the videogame coded with python run on pygame
 # 
 # Autors : Jakub Zak
 # Colaborators : Jakub Senkyr
 #
 # Version : 0.0
+# adding pygame
+import sys
+
+import pygame
+pygame.init()
+
+rozliseni_okna = (800, 600)
+
+okno = pygame.display.set_mode(rozliseni_okna)
+
+while True:
+    for udalost in pygame.event.get():
+        if udalost.type == pygame.QUIT:
+            
+            # adding ability to turn this game off
+            pygame.quit()
+            sys.exit()
+
+    keyword_pressed = pygame.key.get_pressed()        
+
+    # backround
+    okno.fill((255, 255, 255))
+    
+    pygame.display.update()
